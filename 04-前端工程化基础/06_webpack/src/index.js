19601c4aa13fbb1031722769fcd4658349cba5d3
许大仙 <1900919313@qq.com>
2023-10-02 16:47:41 +0800

    : 前端工程化基础

--- 04-前端工程化基础/06_webpack/src/index.js
+++ 04-前端工程化基础/06_webpack/src/index.js
@@ -1,3 +1,9 @@
 const axios = require('axios');
+import {sum} from './utils/math'
 
 console.log(axios)
+
+console.log(3)
+
+console.log(sum(20, 30))
+console.log(sum(20, 31))
---
+++ 04-前端工程化基础/06_webpack/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    // 入口
+    entry: './src/index.js',
+    // 出口
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        // 清理插件
+        new CleanWebpackPlugin(),
+        // 其它插件...
+    ],
+};
