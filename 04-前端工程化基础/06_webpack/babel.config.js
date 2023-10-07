4695aae10e56d64b089857b7c30d6436c75f65d2
许大仙 <1900919313@qq.com>
2023-10-07 15:45:36 +0800

    : 前端工程化基础

--- 04-前端工程化基础/06_webpack/babel.config.js
+++ 04-前端工程化基础/06_webpack/babel.config.js
@@ -2,11 +2,12 @@ module.exports = api => {
     return {
         presets: [
             ["@babel/preset-env", {
-                useBuiltIns: "entry",
+                useBuiltIns: "usage",
                 targets: api.caller(caller => caller && caller.target === "node") ? {node: "current"} : {
                     chrome: "58",
                     ie: "11"
-                }
+                },
+                "corejs": "3"
             }]
         ]
     }
