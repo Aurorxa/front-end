40f812430eff24ac8ca8e080ca6a62e4be1f0f90
许大仙 <1900919313@qq.com>
2023-10-13 16:01:04 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/04_组件通信/04_comp_comm-案例/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {},
+
+}
+</script>
+
+<style lang="less">
+.app {
+ 
+}
+</style>
