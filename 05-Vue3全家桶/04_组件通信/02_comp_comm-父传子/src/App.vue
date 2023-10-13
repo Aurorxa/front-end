58bb1fe1dfc40210ea98ea3853fb406b8bf6b34f
许大仙 <1900919313@qq.com>
2023-10-13 11:14:11 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/04_组件通信/02_comp_comm-父传子/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ShowInfo age="18" height="1.88" name="许大仙"/>
+  <hr>
+  <ShowInfo age="3" height="1.68" name="张三"/>
+</template>
+
+<script>
+
+import ShowInfo from "@/components/ShowInfo.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ShowInfo
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less">
+.app {
+  background-color: #f5f5f5;
+}
+</style>
---
+++ 05-Vue3全家桶/04_组件通信/02_comp_comm-父传子/src/components/ShowInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="infos">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>身高：{{ height }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowInfo",
+  // 作用：接收父组件传递过来的属性
+  // 数组语法
+  props: ["name", "age", "height"],
+  created() {
+    console.log(this.name, this.age, this.height);
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
