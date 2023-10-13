ed684e6aaf9f1817e10e66a29920be826f8046d2
许大仙 <1900919313@qq.com>
2023-10-13 16:24:56 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/04_组件通信/04_comp_comm-案例/src/App.vue
+++ 05-Vue3全家桶/04_组件通信/04_comp_comm-案例/src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="app">
-
+    <TabBar :products="products" @togglePage="currentData"/>
+    <h2 v-if="current">当前页面是：{{ current }}页面</h2>
   </div>
 </template>
 
 <script>
+import TabBar from "@/components/TabBar.vue";
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    TabBar
+  },
   data() {
     return {
-      count: 0
+      products: ["衣服", "鞋子", "裤子"],
+      current: ''
     }
   },
-  methods: {},
+  methods: {
+    currentData(item) {
+      this.current = item;
+    }
+  },
 
 }
 </script>
 
 <style lang="less">
 .app {
- 
+  background-color: pink;
 }
 </style>
