35e15494118075ebc2c408bbd5c6d48ab52f5725
许大仙 <1900919313@qq.com>
2023-10-16 16:08:39 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/06_额外补充/03_keep-alive/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Home组件</h2>
+  <h2>当前计数为：{{ count }}</h2>
+  <button @click="count++">点我+1</button>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
