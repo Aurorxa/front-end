7c8bc52a911f09875576ec9b88a91bd3b2eb912d
许大仙 <1900919313@qq.com>
2023-10-16 15:14:03 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/06_额外补充/01_ref/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <h2 ref="title">{{ message }}</h2>
+    <button @click="change">修改</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      message: 'Hello World',
+      count: 0
+    }
+  },
+  methods: {
+    change() {
+      // 获取 h2 元素
+      console.log('@', this.$refs.title)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.app {
+  background-color: #f5f5f5;
+}
+</style>
