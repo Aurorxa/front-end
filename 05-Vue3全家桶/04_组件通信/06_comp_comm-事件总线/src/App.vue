308079d784c23d7b830de979c56f32740b666458
许大仙 <1900919313@qq.com>
2023-10-16 11:27:04 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/04_组件通信/06_comp_comm-事件总线/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <h2>当前计数：{{ count }}</h2>
+    <Home/>
+    <button @click="handleClick">点我+1</button>
+  </div>
+</template>
+
+<script>
+import Home from "@/components/Home.vue";
+import emitter from "@/utils/event-bus";
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data() {
+    return {
+      count: 10
+    }
+  },
+  methods: {
+    handleClick() {
+      this.count++;
+      emitter.emit('INCREMENT', {count: this.count})
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.app {
+  height: 100px;
+  background-color: #f5f5f5;
+}
+</style>
---
+++ 05-Vue3全家桶/04_组件通信/06_comp_comm-事件总线/src/components/HomeBanner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-banner">
+    HomeBanner
+    {{ count }}
+  </div>
+</template>
+
+<script>
+
+import emitter from "@/utils/event-bus";
+
+export default {
+  name: "HomeBanner",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {},
+  created() {
+    emitter.on('INCREMENT', (e) => {
+      const {count} = e
+      this.count = count
+    })
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.home-banner {
+  padding: 50px;
+  background-color: pink;
+}
+</style>
