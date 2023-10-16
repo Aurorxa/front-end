b63d92fa72d76367db1987906dfb94d0ce14ccaf
许大仙 <1900919313@qq.com>
2023-10-16 08:50:34 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/05_插槽/01_默认插槽/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <ShowMessage content="我是内容" title="我是标题"></ShowMessage>
+    <ShowMessage content="我是内容" title="我是标题"></ShowMessage>
+    <ShowMessage content="我是内容" title="我是标题"></ShowMessage>
+  </div>
+</template>
+
+<script>
+import ShowMessage from "@/components/ShowMessage.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ShowMessage
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less">
+.app {
+  background-color: #f5f5f5;
+}
+</style>
---
+++ 05-Vue3全家桶/05_插槽/01_默认插槽/src/components/ShowMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ title }}</h2>
+  <p>{{ content }}</p>
+</template>
+
+<script>
+export default {
+  name: "ShowMessage",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    content: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
