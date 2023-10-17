de40bcb629d94441b20ae69ed8b2d5aee4b9e5a7
许大仙 <1900919313@qq.com>
2023-10-17 16:08:13 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/03_setup函数中使用computed/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app">
+    <h2>全名：{{ firstName }} {{ lastName }}</h2>
+    <h2>全名：{{ fullName }}</h2>
+    <h2>全名：{{ fullName2 }}</h2>
+  </div>
+</template>
+
+<script>
+
+import {computed, reactive, toRefs} from "vue";
+
+export default {
+  setup() {
+    const names = reactive({
+      firstName: '张',
+      lastName: '三'
+    })
+
+    const fullName = computed(() => names.firstName + names.lastName)
+
+    const fullName2 = computed({
+      set(val) {
+        names.firstName = val.split(' ')[0]
+        names.lastName = val.split(' ')[1]
+      },
+      get() {
+        return names.firstName + names.lastName
+      }
+    })
+
+    return {
+      ...toRefs(names),
+      fullName,
+      fullName2
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
