c54f386a2d808c86afef5b56e875c447334cea2c
许大仙 <1900919313@qq.com>
2023-10-17 16:33:42 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/04_setup函数中使用ref/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2 ref="title">我是标题</h2>
+  <button ref="btn">按钮</button>
+  <hr>
+  <button @click="getElement">获取元素</button>
+</template>
+
+<script>
+import {onMounted, ref} from 'vue'
+
+export default {
+  setup() {
+    const title = ref()
+
+    const btn = ref()
+    const getElement = () => {
+      console.log('@', title.value)
+    }
+
+    onMounted(() => {
+      console.log('@@@@', title, title.value.textContent)
+      console.log('@@@@', btn, btn.value.textContent)
+    })
+
+    return {
+      title,
+      getElement,
+      btn
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
