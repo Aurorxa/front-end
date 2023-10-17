d5054de84e90a6383cfec13f7d6b1a8790571612
许大仙 <1900919313@qq.com>
2023-10-17 09:23:02 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/06_额外补充/05_v-model解析/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>当前计数为：{{ modelValue }}</h2>
+  <button @click="increment">点我+1</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  methods: {
+    increment() {
+      this.$emit("update:modelValue", this.modelValue + 1);
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
