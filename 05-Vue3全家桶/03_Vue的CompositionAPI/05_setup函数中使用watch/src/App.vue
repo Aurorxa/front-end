61a351e989f77b4d4aa8a494fe74f9218358b439
许大仙 <1900919313@qq.com>
2023-10-17 19:51:29 +0800

    : Vue3全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/05_setup函数中使用watch/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>当前计数：{{ count }}</h2>
+  <button @click="count++">点我+1</button>
+  <hr>
+</template>
+
+<script>
+import {ref, watch} from 'vue'
+
+export default {
+  setup() {
+    let count = ref(0)
+
+
+    watch(count, (newValue, oldValue) => {
+      console.log(newValue, oldValue)
+    })
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
