ecac725ed3a4fd5fb24409a4f9717029b73fc6c3
许大仙 <1900919313@qq.com>
2023-10-17 10:54:19 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/01_setup函数实现计数/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h2>当前计数：{{ count }}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    let count = ref(100)
+    const increment = () => {
+      count.value++
+    }
+
+    const decrement = () => {
+      count.value--
+    }
+    return {
+      count,
+      increment,
+      decrement
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
