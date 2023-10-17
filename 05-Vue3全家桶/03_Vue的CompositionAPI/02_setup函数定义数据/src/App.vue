6828f321cb81ea58fc75a73d1309969c2663e4cc
许大仙 <1900919313@qq.com>
2023-10-17 11:22:17 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/03_Vue的CompositionAPI/02_setup函数定义数据/src/App.vue
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/02_setup函数定义数据/src/App.vue
@@ -2,10 +2,14 @@
   <div class="app">
     <h2>{{ message }}</h2>
     <button @click="changeMessage">修改 message</button>
+    <h2>当前计数为：{{ state.count }}</h2>
+    <button @click="increment">点我+1</button>
   </div>
 </template>
 
 <script>
+
+import {reactive} from "vue";
 
 export default {
   setup() {
@@ -13,13 +17,27 @@ export default {
     // 缺点：数据不是响应式的
     let message = "Hello World"
 
+    // 定义响应式的数据
+    // reactive 用于定义复杂的数据
+    let state = reactive({
+      count: 0
+    })
+
     const changeMessage = () => {
       message = "Hello Vue~"
       console.log('changeMessage', message)
     }
+
+    const increment = () => {
+      state.count++
+      console.log('increment', state.count)
+    }
+
     return {
       message,
-      changeMessage
+      state,
+      changeMessage,
+      increment
     }
   }
 }
