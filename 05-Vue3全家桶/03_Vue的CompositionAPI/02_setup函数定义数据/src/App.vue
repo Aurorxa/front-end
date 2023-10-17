59bd49bdf996d139816a026316e9ed0526f1edf8
许大仙 <1900919313@qq.com>
2023-10-17 11:11:22 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/02_setup函数定义数据/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <h2>{{ message }}</h2>
+    <button @click="changeMessage">修改 message</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    // 定义普通的数据：可以正常的被使用
+    // 缺点：数据不是响应式的
+    let message = "Hello World"
+
+    const changeMessage = () => {
+      message = "Hello Vue~"
+      console.log('changeMessage', message)
+    }
+    return {
+      message,
+      changeMessage
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
