407992b5572dd1891abe111a41341d7cc0a9282a
许大仙 <1900919313@qq.com>
2023-10-18 17:17:55 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/03_Vue的CompositionAPI/08_setup语法糖-案例/src/App.vue
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/08_setup语法糖-案例/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-
+  <div class="app">
+    <!-- 区域 header   -->
+    <div class="area-header">
+      <div class="title">{{ data.title }}</div>
+      <div class="subtitle">{{ data.subtitle }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import data from '@/data'
 
-console.log(data)
+console.log('数据', data)
 
 </script>
 
