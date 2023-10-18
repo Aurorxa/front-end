322826d440dbf7cf88bba6a06309852478b9ea37
许大仙 <1900919313@qq.com>
2023-10-18 15:50:49 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/07_setup语法糖/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <h2>{{ message }}</h2>
+    <h2>当前计数：{{ count }}</h2>
+    <button @click="increment">点我+1</button>
+  </div>
+</template>
+
+<script setup>
+// ① 所有编写在顶层中的代码，不会默认暴露给 template 模板
+import {ref} from "vue";
+
+const message = "Hello World"
+
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
