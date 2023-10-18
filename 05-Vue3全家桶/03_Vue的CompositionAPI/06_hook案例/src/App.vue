be6137afd4bb4d04ffc9ddafc5eb3d7ae8e96ec9
许大仙 <1900919313@qq.com>
2023-10-18 14:14:33 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/06_hook案例/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <Home></Home>
+    <hr>
+    <About></About>
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home.vue'
+import About from "@/components/About.vue";
+
+export default {
+  components: {
+    Home,
+    About
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/06_hook案例/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>About组件</h2>
+  <h2>当前计数：{{ count }}</h2>
+  <button @click="increment">点我+1</button>
+  <button @click="decrement">点我-1</button>
+</template>
+
+<script>
+import useCount from '@/hooks/useCount'
+
+export default {
+  setup() {
+    return {
+      ...useCount()
+    }
+  }
+}
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
