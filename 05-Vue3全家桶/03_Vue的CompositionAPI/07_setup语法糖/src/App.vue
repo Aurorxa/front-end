eb7fb37706bb06b5e3ae4dfdb6a4a1dea53fe710
许大仙 <1900919313@qq.com>
2023-10-18 16:10:32 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/03_Vue的CompositionAPI/07_setup语法糖/src/App.vue
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/07_setup语法糖/src/App.vue
@@ -3,12 +3,17 @@
     <h2>{{ message }}</h2>
     <h2>当前计数：{{ count }}</h2>
     <button @click="increment">点我+1</button>
+    <hr>
+    <HelloWorld :age="18" name="许大仙" @show-btn-click-event="showBtnClickEvent"></HelloWorld>
   </div>
 </template>
 
 <script setup>
 // ① 所有编写在顶层中的代码，都会默认暴露给 template 模板
 import {ref} from "vue";
+
+// ② 导入的组件直接使用
+import HelloWorld from "@/components/HelloWorld.vue";
 
 const message = "Hello World"
 
@@ -17,6 +22,9 @@ const increment = () => {
   count.value++
 }
 
+const showBtnClickEvent = (...args) => {
+  console.log(args)
+}
 </script>
 
 <style lang="less" scoped>
---
+++ 05-Vue3全家桶/03_Vue的CompositionAPI/07_setup语法糖/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>HelloWorld 组件</h2>
+    <h2>姓名是：{{ name }} ，年龄是：{{ age }}</h2>
+    <button @click="showBtnClick">Show</button>
+  </div>
+</template>
+
+<script setup>
+// 定义 props
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+})
+console.log("@", props.name, props.age)
+
+// 绑定函数，并发出自定义事件
+// 声明事件
+const emits = defineEmits(['showBtnClickEvent'])
+const showBtnClick = () => {
+  emits('showBtnClickEvent', props.name, props.age)
+}
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
