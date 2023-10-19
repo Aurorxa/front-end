f65813592d2d0e289bf6fe3a2ca0763d5a34b7f8
许大仙 <1900919313@qq.com>
2023-10-19 15:02:53 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/App.vue
+++ 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/App.vue
@@ -7,13 +7,13 @@
           通过传递 `to` 来指定链接
           `<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签
       -->
-      <router-link to="/home">Home组件</router-link>
+      <router-link to="/home">首页</router-link>
       <br>
-      <router-link to="/about">About组件</router-link>
+      <router-link to="/about">关于</router-link>
       <br>
-      <router-link to="/user/1">User组件-1用户</router-link>
+      <router-link to="/user/1">1用户</router-link>
       <br>
-      <router-link to="/user/2">User组件-2用户</router-link>
+      <router-link to="/user/2">2用户</router-link>
     </div>
     <br>
     <!-- 路由出口：路由匹配到的组件将渲染在这里 -->
--- 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/components/User.vue
+++ 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/components/User.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="user">
-    我是 User 组件
+    <!-- 在模板中获取 id   -->
+    <h2>我是{{ $route.params.id }}用户</h2>
   </div>
 </template>
 
 <script setup>
+import {onBeforeRouteUpdate, useRoute} from "vue-router";
+import {watch, watchEffect} from "vue";
+
+const route = useRoute()
+
+watch(() => route.params, (params) => {
+  console.log('watch @', params)
+}, {
+  immediate: true
+})
+
+watchEffect(() => {
+  console.log('watchEffect #', route.params)
+})
+
+onBeforeRouteUpdate((to, from) => {
+  console.log('onBeforeRouteUpdate #', to, from)
+})
 
 </script>
 
