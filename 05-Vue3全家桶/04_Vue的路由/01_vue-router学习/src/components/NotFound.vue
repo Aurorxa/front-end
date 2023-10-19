1cccda2b4770bd5687eb216cabaee0127ab60c9f
许大仙 <1900919313@qq.com>
2023-10-19 15:15:49 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/components/NotFound.vue
+++ 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/components/NotFound.vue
@@ -1,9 +1,19 @@
 <template>
-  <h2 style="color:red">404，找不到回家的路</h2>
+  <h2 style="color:red">
+    404，找不到回家的路
+
+    {{ computedPath }} 不正确
+  </h2>
 </template>
 
 <script setup>
+import {computed} from "vue";
+import {useRoute} from "vue-router";
 
+const route = useRoute()
+const computedPath = computed(() => {
+  return route.params.pathMatch
+})
 </script>
 
 <style lang="less" scoped>
