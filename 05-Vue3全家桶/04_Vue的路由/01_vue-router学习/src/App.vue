35116cd61ea516111ee2cc70fee887c4018fea23
许大仙 <1900919313@qq.com>
2023-10-19 13:50:50 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/App.vue
+++ 05-Vue3全家桶/04_Vue的路由/01_vue-router学习/src/App.vue
@@ -8,6 +8,7 @@
           `<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签
       -->
       <router-link to="/home">Home组件</router-link>
+      <br>
       <router-link to="/about">About组件</router-link>
     </div>
     <!-- 路由出口：路由匹配到的组件将渲染在这里 -->
