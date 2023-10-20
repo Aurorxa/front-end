7971bc8558f2b5ce3ad00dc0457384f450a51ccc
许大仙 <1900919313@qq.com>
2023-10-20 10:23:43 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <h2>当前计数为：{{ count }}</h2>
+    <button @click="handleClick">点我+1</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+
+import {useStore} from "vuex";
+import {computed} from "vue";
+
+const store = useStore()
+
+const count = computed(() => {
+  return store.state.count
+})
+
+const handleClick = () => {
+  store.commit("increment")
 }
 
-nav {
-  padding: 30px;
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="less">
+.app {
+  background-color: pink;
 }
 </style>
--- 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/store/index.js
+++ 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/store/index.js
@@ -1,14 +1,17 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state() {
+        return {
+            count: 0
+        }
+    },
+    getters: {},
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    },
+    actions: {},
+    modules: {}
 })
