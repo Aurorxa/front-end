3c872df63346b7354c1bba130836095c335db5c5
许大仙 <1900919313@qq.com>
2023-10-20 11:25:37 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="app">
     <h2>App 组件，当前计数为：{{ count }}</h2>
+    <h2>App 组件，名字：{{ name }}</h2>
+    <h2>App 组件，年龄：{{ age }}</h2>
     <Home/>
   </div>
 </template>
 
 <script setup>
-import {computed, isRef} from "vue";
-import {useStore} from "vuex";
+import {computed} from "vue";
+import {mapState, useStore} from "vuex";
 import Home from "@/components/Home.vue";
 
 const store = useStore()
@@ -16,7 +18,8 @@ const count = computed(() => {
   return store.state.count
 })
 
-console.log('@', isRef(count))
+const name = computed(mapState(["name"]).name.bind({$store: store}))
+const age = computed(mapState(["age"]).age.bind({$store: store}))
 
 </script>
 
--- 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/store/index.js
+++ 05-Vue3全家桶/05_Vue的状态管理/01_vue_vuex/src/store/index.js
@@ -3,10 +3,22 @@ import {createLogger, createStore} from 'vuex'
 export default createStore({
     state() {
         return {
-            count: 0
+            count: 0,
+            name: "许大仙",
+            age: 18
         }
     },
-    getters: {},
+    getters: {
+        getCount(state) {
+            return state.count
+        },
+        getName(state) {
+            return state.name
+        },
+        getAge(state) {
+            return state.age
+        }
+    },
     mutations: {
         increment(state) {
             state.count++
