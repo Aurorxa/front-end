00adc8dfac0ba5bfcff9314597d7ebcab695cf48
许大仙 <1900919313@qq.com>
2023-10-23 10:09:13 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/components/User.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/components/User.vue
@@ -5,6 +5,9 @@
       <h2>年龄：{{ age }}</h2>
       <h2>身高：{{ height }}</h2>
     </div>
+    <button @click="changeName">修改姓名</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="resetState">重置状态</button>
   </div>
 </template>
 
@@ -15,6 +18,24 @@ import {toRefs} from "vue";
 const useStore = useUserStore()
 
 const {name, age, height} = toRefs(useStore)
+
+const changeName = () => {
+  useStore.name = "李四"
+}
+
+const changeAge = () => {
+  useStore.$patch((state) => {
+    state.age++
+    state.hasChanged = true
+  })
+}
+
+console.log('@@@', useStore)
+
+const resetState = () => {
+  useStore.$reset()
+}
+
 </script>
 
 <style lang="less" scoped>
