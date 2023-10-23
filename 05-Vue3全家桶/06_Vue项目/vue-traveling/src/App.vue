49a5acb3d2e41104f1ac2b4de7175aa1bcfde012
许大仙 <1900919313@qq.com>
2023-10-23 17:01:53 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-
+  <div class="app"></div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
-
+.app {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
 </style>
