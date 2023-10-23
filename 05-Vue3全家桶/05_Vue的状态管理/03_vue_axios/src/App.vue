69633697b29596801600317e804c816c8d30005e
许大仙 <1900919313@qq.com>
2023-10-23 13:52:16 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="app">
-
+    <button @click="axiosRequest">Axios Request</button>
   </div>
 </template>
 
-<script>
+<script setup>
+import axios from "axios";
+
+const axiosRequest = () => {
+  axios.request({
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    console.log(res)
+  });
+}
 </script>
 
 <style lang="less">
