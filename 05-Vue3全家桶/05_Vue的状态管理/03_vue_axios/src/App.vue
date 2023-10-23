9823c44db60127b4dd7a4616b4ad7fe483a0829e
许大仙 <1900919313@qq.com>
2023-10-23 14:11:57 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
@@ -7,7 +7,8 @@
       <button @click="axiosGET">Axios GET</button>
     </div>
     <div>
-      <button @click="axiosPOST">Axios POST</button>
+      <button @click="axiosPOST1">Axios POST1</button>
+      <button @click="axiosPOST2">Axios POST2</button>
     </div>
   </div>
 </template>
@@ -42,10 +43,29 @@ const axiosGET = () => {
   });
 }
 
-const axiosPOST = () => {
+const axiosPOST1 = () => {
   axios.post("https://jsonplaceholder.typicode.com/comments", {
     body: "This is a comment",
     postId: 1,
+  }, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    console.log(res)
+    console.log('data', res.data)
+  });
+}
+
+const axiosPOST2 = () => {
+  axios.post("https://jsonplaceholder.typicode.com/comments", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: {
+      body: "This is a comment",
+      postId: 1,
+    }
   }).then((res) => {
     console.log(res)
     console.log('data', res.data)
