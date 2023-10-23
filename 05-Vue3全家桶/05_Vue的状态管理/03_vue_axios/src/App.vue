24ffcb182ed137ae6f180436bf8fc8f860ac0a40
许大仙 <1900919313@qq.com>
2023-10-23 13:57:05 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/03_vue_axios/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app">
-    <button @click="axiosRequest">Axios Request</button>
+    <div>
+      <button @click="axiosRequest">Axios Request</button>
+    </div>
+    <div>
+      <button @click="axiosGET">Axios GET</button>
+    </div>
   </div>
 </template>
 
@@ -16,8 +21,24 @@ const axiosRequest = () => {
     },
   }).then((res) => {
     console.log(res)
+    console.log('data', res.data)
   });
 }
+
+const axiosGET = () => {
+  axios.get("https://jsonplaceholder.typicode.com/comments", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    params: {
+      postId: 1,
+    },
+  }).then((res) => {
+    console.log(res)
+    console.log('data', res.data)
+  });
+}
+
 </script>
 
 <style lang="less">
