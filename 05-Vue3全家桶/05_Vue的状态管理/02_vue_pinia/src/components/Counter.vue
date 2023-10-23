45d280acae94eeaf591192398945462b7724b0be
许大仙 <1900919313@qq.com>
2023-10-23 09:30:45 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/components/Counter.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/components/Counter.vue
@@ -1,13 +1,16 @@
 <template>
-  <h2>当前计数为：{{ counterStore.count }}</h2>
-  <h2>2倍计数为：{{ counterStore.doubleCount }}</h2>
-  <button @click="counterStore.increment()">点我+1</button>
+  <h2>当前计数为：{{ count }}</h2>
+  <h2>2倍计数为：{{ doubleCount }}</h2>
+  <button @click="increment()">点我+1</button>
 </template>
 
 <script setup>
 import useCounterStore from "@/store/counter";
+import {toRefs} from "vue";
 
 const counterStore = useCounterStore();
+
+const {count, doubleCount, increment} = toRefs(counterStore);
 
 </script>
 
