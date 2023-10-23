4be4511c7663d7988dbcdec7528035a6b50ea5ac
许大仙 <1900919313@qq.com>
2023-10-23 09:13:30 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/App.vue
+++ 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="app">
-    <h2>当前计数为：{{ useCounter.count }}</h2>
-    <h2>2倍计数为：{{ useCounter.double }}</h2>
-    <button @click="increment">点我+1</button>
+    <Counter></Counter>
   </div>
 </template>
 
 <script setup>
-import {useCounterStore} from "@/store";
-
-const useCounter = useCounterStore();
-
-const increment = () => {
-  useCounter.increment()
-}
-
+import Counter from "@/components/Counter.vue";
 </script>
 
 <style lang="less">
---
+++ 05-Vue3全家桶/05_Vue的状态管理/02_vue_pinia/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>当前计数为：{{ useCounter.count }}</h2>
+  <h2>2倍计数为：{{ useCounter.double }}</h2>
+  <button @click="useCounter.increment()">点我+1</button>
+</template>
+
+<script setup>
+import {useCounterStore} from "@/store";
+
+const useCounter = useCounterStore();
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
