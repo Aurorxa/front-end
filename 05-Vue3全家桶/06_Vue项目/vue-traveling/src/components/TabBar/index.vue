1d97a88a348772e1141f596f28e91c8269a45936
许大仙 <1900919313@qq.com>
2023-10-26 10:06:39 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/components/TabBar/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/components/TabBar/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="tab-bar">
-    <div class="tab-bar-item">首页</div>
-    <div class="tab-bar-item">收藏</div>
-    <div class="tab-bar-item">订单</div>
-    <div class="tab-bar-item">消息</div>
+    <div class="tab-bar-item" @click="$router.push('/home')">
+      <img alt="首页" src="@/assets/tab-bar/tab_home.png">
+      首页
+    </div>
+    <div class="tab-bar-item" @click="$router.push('/favor')">收藏</div>
+    <div class="tab-bar-item" @click="$router.push('/order')">订单</div>
+    <div class="tab-bar-item" @click="$router.push('/message')">消息</div>
   </div>
 </template>
 
@@ -17,7 +20,7 @@
   bottom: 0;
   left: 0;
   right: 0;
-  height: 50px;
+  height: 44px;
   display: flex;
   background-color: pink;
 
@@ -26,6 +29,10 @@
     display: flex;
     justify-content: center;
     align-items: center;
+
+    img {
+      width: 22px;
+    }
   }
 }
 </style>
