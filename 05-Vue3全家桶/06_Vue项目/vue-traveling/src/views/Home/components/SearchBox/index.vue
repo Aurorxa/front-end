99ebfa84b0be65d1d488174d43c7ea178eaa7b85
许大仙 <1900919313@qq.com>
2023-10-26 17:07:08 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/Home/components/SearchBox/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/Home/components/SearchBox/index.vue
@@ -2,7 +2,7 @@
   <div class="search-box">
     <!-- 定位   -->
     <van-row class="location" justify="space-around">
-      <van-col class="item city" span="10">江苏</van-col>
+      <van-col class="item city" span="10">{{ currentCity }}</van-col>
       <van-col class="item position" span="8" @click="positionClick">
         <span class="text">我的位置</span>
         <van-icon class="icon" name="location" size="18"/>
@@ -12,18 +12,46 @@
 </template>
 
 <script setup>
+import {ref} from "vue";
+import {showNotify} from "vant";
+import 'vant/es/notify/style'
+
+const currentCity = ref('北京')
 // 获取位置、城市
-const positionClick = () => {
-  console.log('@@@')
-  navigator.geolocation.getCurrentPosition((res) => {
-    console.log("获取位置成功", res)
-  }, err => {
-    console.log("获取位置失败", err)
-  }, {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  })
+const positionClick = async () => {
+  const getLocation = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve(position);
+      }, error => {
+        reject(error);
+      }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      });
+    })
+  }
+
+  try {
+    const location = await getLocation()
+    const {coords: {latitude, longitude}} = location
+    const fetchResult = await fetch(`https://api.oioweb.cn/api/ip/geocoder?lng=${longitude}&lat=${latitude}`)
+    const {code, result: {address_component: {province, city}}} = await fetchResult.json()
+    if (code === 200) {
+      showNotify(
+          {
+            type: 'primary', message: `${province} ${city}`
+          });
+      currentCity.value = `${province} ${city}`
+    }
+  } catch (e) {
+    showNotify(
+        {
+          type: 'danger', message: `获取位置失败`
+        });
+    console.log("获取位置失败", e)
+  }
 
 }
 </script>
