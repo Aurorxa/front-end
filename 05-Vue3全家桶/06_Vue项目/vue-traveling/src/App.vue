9722c4c9cb3444ab7eabc0f487dad02315e57486
许大仙 <1900919313@qq.com>
2023-10-26 09:34:50 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <!--    -->
+    <!-- 路由出口：路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
-    <router-link to="/">首页</router-link>
-    <br>
-    <router-link to="/favor">收藏</router-link>
-    <br>
-    <router-link to="/order">订单</router-link>
-    <br>
-
-    <router-link to="/message">消息</router-link>
-    <br>
+    <div class="tab-bar">
+      <router-link to="/">首页</router-link>
+      <br>
+      <router-link to="/favor">收藏</router-link>
+      <br>
+      <router-link to="/order">订单</router-link>
+      <br>
+      <router-link to="/message">消息</router-link>
+      <br>
+    </div>
   </div>
 </template>
 
