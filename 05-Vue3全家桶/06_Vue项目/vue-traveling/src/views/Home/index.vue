c1e251678216dd7f4072944d58bbe91029234aba
许大仙 <1900919313@qq.com>
2023-10-26 15:37:52 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/Home/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/Home/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="app">
-    Home组件
+  <div class="home">
+    <!--  标题  -->
+    <van-nav-bar title="人在旅途"/>
+    <!--  轮播图 -->
+    <van-swipe :autoplay="3000" class="swipe" indicator-color="white">
+      <van-swipe-item>
+        <img alt="" class="pic" src="@/assets/images/home/banner.webp">
+      </van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
@@ -9,5 +19,32 @@
 </script>
 
 <style lang="less" scoped>
+.home {
 
+  :deep(.van-nav-bar__title) {
+    color: var(--primary-color);
+  }
+
+  .swipe {
+    
+    :deep(.van-swipe-item) {
+      color: #fff;
+      font-size: 20px;
+      line-height: 150px;
+      height: 150px;
+      text-align: center;
+      background-color: #39a9ed;
+
+      .pic {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
