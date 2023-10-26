28a9209857693d27d766a7bfb8901144b70ecb0a
许大仙 <1900919313@qq.com>
2023-10-26 14:35:07 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/App.vue
@@ -2,13 +2,12 @@
   <div class="app">
     <!-- 路由出口：路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
-    <!--  导航栏组件  -->
     <TabBar></TabBar>
   </div>
 </template>
 
 <script setup>
-import TabBar from '@/components/TabBar/index.vue'
+import TabBar from "@/components/TabBar/index.vue";
 </script>
 
 <style scoped>
--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/components/TabBar/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/components/TabBar/index.vue
@@ -1,67 +1,29 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item,index) in tabBarData" :key="item.path">
-      <div
-          :class="{'active': currentIndex === index}"
-          class="tab-bar-item"
-          @click="handleClickTab(item.path,index)"
-      >
-        <img v-if="index !== currentIndex" :alt="item.text" :src="getAssetsUrl(item.image)" class="pic">
-        <img v-else :alt="item.text" :src="getAssetsUrl(item.activeImage)" class="pic">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ee0a24" route>
+      <template v-for="(item,index) in tabBarData" :key="index">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon="props">
+            <img :alt="item.text" :src="props.active ? getAssetsUrl(item.activeImage) : getAssetsUrl(item.image)"/>
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
 
 import tabBarData from '@/assets/data/tabBar'
+
+import {ref} from "vue"
 import {getAssetsUrl} from "@/utils/loadAssets.js";
-import {ref} from "vue";
-import {useRouter} from "vue-router";
 
 const currentIndex = ref(0)
-
-const router = useRouter()
-const handleClickTab = (path, index) => {
-  router.push(path)
-  currentIndex.value = index
-}
 
 </script>
 
 <style lang="less" scoped>
-.tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 44px;
-  display: flex;
-  border-top: 1px solid #f3f3f3;
 
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    .pic {
-      width: 22px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
-
-    &.active {
-      color: var(--primary-color);
-    }
-  }
-
-
-}
 </style>
