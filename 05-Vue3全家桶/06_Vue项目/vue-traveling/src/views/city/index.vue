a07776b392f04ad2b08d4ae45555b4e686e1b5c4
许大仙 <1900919313@qq.com>
2023-10-27 16:08:06 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/city/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/city/index.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="city">
-    <!--  搜索  -->
-    <van-search
-        v-model="searchValue"
-        placeholder="城市/区域/位置"
-        shape="round"
-        show-action
-        @cancel="onCancel"
-        @search="onSearch"
-    />
-    <!-- 标签页 -->
-    <van-tabs v-model:active="activeTab" animated swipeable>
-      <template v-for="(value, key) in allCities" :key="key">
-        <van-tab :title="value.title">{{ value.title }}</van-tab>
+    <!-- 头部 -->
+    <div class="header">
+      <!--  搜索  -->
+      <van-search
+          v-model="searchValue"
+          placeholder="城市/区域/位置"
+          shape="round"
+          show-action
+          @cancel="onCancel"
+          @search="onSearch"
+      />
+      <!-- 标签页 -->
+      <van-tabs v-model:active="activeTab" ellipsis rendered="renderedTab" @change="changeTab"
+                @click-tab="clickTab">
+        <template v-for="(value, key) in allCities" :key="key">
+          <van-tab :name="key" :title="value.title">{{ value.title }}</van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <!--  列表数据  -->
+    <div class="content">
+      <template v-for="item in 1000">
+        <div> {{ item }}</div>
       </template>
-    </van-tabs>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue"
-import {showNotify} from "vant"
+import {ref, watchEffect} from "vue"
+import {showNotify, showToast} from "vant"
 import {useRouter} from "vue-router"
 import {useCityStore} from "@/stores";
 import {storeToRefs} from "pinia";
 
 const searchValue = ref('')
-const activeTab = ref(0);
+const activeTab = ref('');
 const router = useRouter()
 
 const onSearch = (val) => {
@@ -46,8 +56,28 @@ const cityStore = useCityStore()
 cityStore.fetchAllCity()
 
 const {allCities} = storeToRefs(cityStore)
+
+watchEffect(() => {
+  activeTab.value = Object.keys(allCities.value).shift()
+})
+
+const changeTab = ({title, name}) => {
+  console.log('@@@@', 'changeTab')
+  showToast('@@@  changeTab' + title + name)
+}
+const clickTab = ({title, name}) => {
+  console.log('@@@@', 'clickTab')
+  showToast('@@@  clickTab' + title + name)
+}
+
 </script>
 
 <style lang="less" scoped>
+.city {
+  .header {
+    position: sticky;
+    top: 0;
+  }
 
+}
 </style>
