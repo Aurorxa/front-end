8d25c1dd230aecb82aec4c341a50424422af9553
许大仙 <1900919313@qq.com>
2023-10-29 07:05:53 +0800

    : Vue3全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/city/components/city-group/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/city/components/city-group/index.vue
@@ -1,6 +1,6 @@
 <template>
   <!--  城市分组    -->
-  <van-index-bar highlight-color="#ff9854">
+  <van-index-bar :index-list="cities.map((city) => city.group)" highlight-color="#ff9854">
     <template v-for="(city) in cities">
       <van-index-anchor :index="city.group">{{ city.group }}</van-index-anchor>
       <template v-for="item in city?.cities">
