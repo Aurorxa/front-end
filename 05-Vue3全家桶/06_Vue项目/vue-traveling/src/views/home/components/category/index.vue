80fd7f2b2d33d7627b921d75c85b4825d8903931
许大仙 <1900919313@qq.com>
2023-10-30 17:27:43 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/home/components/category/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="category">
+    <template v-for="(item,index) in categories" :key="index">
+      <div class="item">
+        <img :src="item.pictureUrl" alt="" class="pic">
+        <span class="title">{{ item.title }}</span>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+import {useHomeStore} from "@/stores/index.js";
+import {storeToRefs} from "pinia";
+
+const homeStore = useHomeStore()
+
+const {categories} = storeToRefs(homeStore)
+
+console.log('@@@', categories)
+</script>
+
+<style lang="less" scoped>
+.category {
+  margin-top: 20px;
+  padding: 0 40px;
+
+  display: flex;
+  overflow-x: auto;
+
+  .item {
+    display: flex;
+    width: 70px;
+    flex-shrink: 0;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .pic {
+      width: 32px;
+      height: 32px;
+    }
+
+    .title {
+      font-size: 13px;
+    }
+
+  }
+}
+</style>
--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/home/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/home/index.vue
@@ -13,12 +13,14 @@
     </van-swipe>
     <!-- 搜索  -->
     <SearchBox></SearchBox>
-    <div>其他</div>
+    <!-- 分类   -->
+    <Category></Category>
   </div>
 </template>
 
 <script setup>
 import SearchBox from '@/views/Home/components/search-box/index.vue'
+import Category from '@/views/Home/components/category/index.vue'
 import {useHomeStore} from "@/stores/index.js"
 // 从 store 中获取数据
 const homeStore = useHomeStore()
