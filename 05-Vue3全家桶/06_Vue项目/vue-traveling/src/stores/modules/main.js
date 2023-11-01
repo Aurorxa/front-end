83470aba17ce239c01aadc149eee9c64b26148b6
许大仙 <1900919313@qq.com>
2023-11-01 10:08:33 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/stores/modules/main.js
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/stores/modules/main.js
@@ -1,7 +1,11 @@
 import {defineStore} from "pinia"
+import dayjs from "dayjs"
 
 export const useMainStore = defineStore('main', {
-    state: () => ({}),
+    state: () => ({
+        startDate: new Date(),
+        endDate: dayjs().add(1, 'day'),
+    }),
     actions: {},
     getters: {},
     persist: true,
