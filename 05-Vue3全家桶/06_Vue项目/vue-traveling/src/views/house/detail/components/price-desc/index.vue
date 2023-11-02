4a93db0e0e9b8d2372566731dde01fd3fe9144ab
许大仙 <1900919313@qq.com>
2023-11-02 17:26:25 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/price-desc/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/price-desc/index.vue
@@ -3,6 +3,7 @@
     <h2 class="title">{{ priceDesc.title }}</h2>
     <van-text-ellipsis
         :content="priceDesc.introduction"
+        class="content"
         collapse-text="收起"
         expand-text="展开"
         rows="3"
@@ -25,15 +26,18 @@ console.log('price desc', props.priceDesc)
 .price-desc {
   width: 100%;
   height: 300px;
-
-  padding: 16px 12px;
-  border-top: 5px solid #f2f3f4;
+  padding: 0 15px;
+  margin-top: 120px;
 
   .title {
     font-size: 14px;
     color: #000;
     font-weight: 700;
     margin-bottom: 10px;
+  }
+
+  .content {
+    padding: 0 30px 0 0;
   }
 
 }
