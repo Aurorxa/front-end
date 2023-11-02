fe81d407aef861f298bc405aecf1b44fa3d022e9
许大仙 <1900919313@qq.com>
2023-11-02 17:29:36 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/price-desc/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/price-desc/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="price-desc">
-    <h2 class="title">{{ priceDesc.title }}</h2>
-    <van-text-ellipsis
-        :content="priceDesc.introduction"
-        class="content"
-        collapse-text="收起"
-        expand-text="展开"
-        rows="3"
-    />
+    <van-row>
+      <van-col span="22">
+        <h2 class="title">{{ priceDesc.title }}</h2>
+      </van-col>
+      <van-col span="22">
+        <van-text-ellipsis
+            :content="priceDesc.introduction"
+            class="content"
+            collapse-text="收起"
+            expand-text="展开"
+            rows="3"
+        />
+      </van-col>
+    </van-row>
+
   </div>
 </template>
 
@@ -25,7 +32,6 @@ console.log('price desc', props.priceDesc)
 <style lang="less" scoped>
 .price-desc {
   width: 100%;
-  height: 300px;
   padding: 0 15px;
   margin-top: 120px;
 
@@ -34,10 +40,6 @@ console.log('price desc', props.priceDesc)
     color: #000;
     font-weight: 700;
     margin-bottom: 10px;
-  }
-
-  .content {
-    padding: 0 30px 0 0;
   }
 
 }
