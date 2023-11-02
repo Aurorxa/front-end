e920ef5f7ff6c725c1cb8667eb9614bbbb8b7574
许大仙 <1900919313@qq.com>
2023-11-02 13:34:16 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/swipe/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/swipe/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="swipe">
-    <van-swipe :autoplay="3000" lazy-render>
+    <van-swipe :autoplay="3000">
       <template v-for="(item,index) in swipeData" :key="index">
         <van-swipe-item>
           <img
@@ -12,19 +12,60 @@
       </template>
       <!-- 自定义指示器 -->
       <template #indicator="{ active, total }">
-        <div class="custom-indicator">{{ active + 1 }}/{{ total }}</div>
+        <div class="custom-indicator">
+          <template v-for="(value,key) in swipeGroup" :key="key">
+            <span :class="{active: isActive(value,active)}" class="item">
+              {{ computedCurrentItemTitle(value, active) }}
+            </span>
+          </template>
+        </div>
       </template>
     </van-swipe>
   </div>
 </template>
 
 <script setup>
+import underscore from 'underscore'
+
 const props = defineProps({
   swipeData: {
     type: Array,
     default: []
   }
 })
+
+const swipeGroup = underscore.groupBy(props.swipeData, ({enumPictureCategory}) => {
+  switch (enumPictureCategory) {
+    case 2 :
+      return 'bedchamber'
+    case 4 :
+      return 'toilet'
+    default:
+      return 'other'
+  }
+})
+
+const isActive = (value, active) => {
+  return value.some(({orderIndex}) => orderIndex === active)
+}
+
+const computedCurrentItem = (value, active) => {
+  let currentIndex = value.findIndex(({orderIndex}) => orderIndex === active)
+
+  return {
+    title: value.at(currentIndex).title,
+    currentIndex: currentIndex < 0 ? 1 : currentIndex + 1,
+    total: value.length,
+  }
+}
+
+const computedCurrentItemTitle = (value, active) => {
+  const {title, currentIndex, total} = computedCurrentItem(value, active)
+  return `${title} ${currentIndex} / ${total}`
+}
+
+
+console.log('swipe', swipeGroup)
 </script>
 
 <style lang="less" scoped>
@@ -49,7 +90,18 @@ const props = defineProps({
     padding: 2px 5px;
     font-size: 12px;
     color: #fff;
-    background: rgba(0, 0, 0, 0.1);
+    background: rgba(0, 0, 0, 0.6);
+
+    .item {
+      margin: 0 2px;
+    }
+
+    .active {
+      background-color: #fff;
+      color: #000;
+      border-radius: 5px;
+      padding: 0 1px;
+    }
   }
 }
 </style>
