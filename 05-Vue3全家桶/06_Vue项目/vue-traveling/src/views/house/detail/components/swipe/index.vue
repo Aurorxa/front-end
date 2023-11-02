d4a22dbb9ecf21bb9a074596f48c21ca82925684
许大仙 <1900919313@qq.com>
2023-11-02 10:17:58 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/swipe/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="swipe">
+    <van-swipe :autoplay="3000" lazy-render>
+      <van-swipe-item v-for="(item,index) in swipeData" :key="index">
+        <img
+            :alt="item.url"
+            :src="item.url"
+            class="pic"
+        />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  swipeData: {
+    type: Array,
+    default: []
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.swipe {
+
+  :deep(.van-swipe-item) {
+    color: #fff;
+    font-size: 20px;
+    height: 200px;
+    text-align: center;
+
+    .pic {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/index.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="house-detail">
+    <!-- 导航条 -->
     <van-nav-bar
         left-arrow
         left-text="返回"
         title="房屋详情"
         @click-left="handleClickLeft"
     />
+    <!-- 轮播图 -->
+    <div v-if="mainPart" class="swipe">
+      <HouseDetailSwipe :swipe-data="mainPart?.topModule?.housePicture?.housePics"></HouseDetailSwipe>
+    </div>
+
   </div>
 </template>
 
 <script setup>
 import {useRoute, useRouter} from "vue-router"
 import {useHouseStore} from "@/stores/index.js"
+import {storeToRefs} from "pinia"
+import {computed} from "vue"
+import HouseDetailSwipe from '@/views/house/detail/components/swipe/index.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -26,8 +35,15 @@ const handleClickLeft = () => {
 // 获取数据
 houseStore.fetchHouseDetail(id)
 
+const {detail} = storeToRefs(houseStore)
+
+const mainPart = computed(() => detail.value.mainPart)
 </script>
 
 <style lang="less" scoped>
-
+.house-detail {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
 </style>
