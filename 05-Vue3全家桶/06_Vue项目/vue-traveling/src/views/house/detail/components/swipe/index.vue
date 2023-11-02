9e65ef850792b76b90a4d5c6f7bc371f4809adba
许大仙 <1900919313@qq.com>
2023-11-02 10:36:32 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/swipe/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/swipe/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="swipe">
     <van-swipe :autoplay="3000" lazy-render>
-      <van-swipe-item v-for="(item,index) in swipeData" :key="index">
-        <img
-            :alt="item.url"
-            :src="item.url"
-            class="pic"
-        />
-      </van-swipe-item>
+      <template v-for="(item,index) in swipeData" :key="index">
+        <van-swipe-item>
+          <img
+              :alt="item.url"
+              :src="item.url"
+              class="pic"
+          />
+        </van-swipe-item>
+      </template>
+      <!-- 自定义指示器 -->
+      <template #indicator="{ active, total }">
+        <div class="custom-indicator">{{ active + 1 }}/{{ total }}</div>
+      </template>
     </van-swipe>
   </div>
 </template>
@@ -24,7 +30,7 @@ const props = defineProps({
 <style lang="less" scoped>
 .swipe {
 
-  :deep(.van-swipe-item) {
+  .van-swipe-item {
     color: #fff;
     font-size: 20px;
     height: 200px;
@@ -34,6 +40,16 @@ const props = defineProps({
       width: 100%;
       height: 100%;
     }
+  }
+
+  .custom-indicator {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    padding: 2px 5px;
+    font-size: 12px;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.1);
   }
 }
 </style>
