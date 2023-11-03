5f21c566944f49ea661f688648b0e88e683beda6
许大仙 <1900919313@qq.com>
2023-11-03 12:40:16 +0800

    Vue3 全家桶

--- 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/action-bar/index.vue
+++ 05-Vue3全家桶/06_Vue项目/vue-traveling/src/views/house/detail/components/action-bar/index.vue
@@ -2,7 +2,7 @@
   <div class="house-action-bar">
     <van-action-bar>
       <van-action-bar-icon icon="chat-o" text="聊天"/>
-      <van-action-bar-icon icon="chat-o" text="聊天"/>
+      <van-action-bar-icon icon="cart-o" text="购物车"/>
       <van-action-bar-button color="#ff9854" text="预约当前房源" type="warning"/>
     </van-action-bar>
   </div>
