e5798eb1e05f53cec86d612e3437b7246df97a2a
许大仙 <1900919313@qq.com>
2023-11-06 09:45:05 +0800

    Vue3 全家桶

---
+++ 05-Vue3全家桶/07_Vue高级语法补充/vue-animation/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app">
+    <button @click="change">切换</button>
+    <Transition>
+      <HelloWorld v-if="isShow"/>
+    </Transition>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue"
+import {ref} from "vue"
+
+const isShow = ref(false)
+
+const change = () => {
+  isShow.value = !isShow.value
+}
+</script>
+
+
+<style scoped>
+.v-enter-from {
+  opacity: 0;
+  transform: scale(0.5);
+}
+
+.v-leave-to {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.v-enter-active {
+  transition: all 2s ease;
+
+}
+
+</style>
