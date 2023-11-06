bb6735b9cbf79e27ede1c04f0abeca51e988fcb4
许大仙 <1900919313@qq.com>
2023-11-06 15:16:37 +0800

    Vue3 全家桶

---
+++ 06-TypeScript/01_初识TypeScript/02_TypeScript的类型检测.ts
@@ -0,0 +1,9 @@
+function getLength(args: string[] | string) {
+    return args.length
+}
+
+console.log(getLength("Hello World"))
+console.log(getLength(["a", "b", "c"]))
+// console.log(getLength(123))
+
+export {}
