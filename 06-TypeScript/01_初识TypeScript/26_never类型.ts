e101a1bd1ef0508e971c34ebd8582de55118da97
许大仙 <1900919313@qq.com>
2023-11-07 17:04:23 +0800

    Vue3 全家桶

---
+++ 06-TypeScript/01_初识TypeScript/26_never类型.ts
@@ -0,0 +1,23 @@
+function handleMessage(message: string | number | boolean) {
+    switch (typeof message) {
+        case'string':
+            console.log(message.length)
+            break
+        case 'number':
+            console.log(message)
+            break
+        case 'boolean':
+            console.log(Number(message))
+            break
+        default:
+            const check: never = message
+            console.log('@@@', check)
+            break
+    }
+}
+
+handleMessage("abc")
+handleMessage(123)
+handleMessage(true)
+
+export {}
