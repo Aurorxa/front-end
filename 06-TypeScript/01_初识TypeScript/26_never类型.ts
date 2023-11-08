c0a92b4e50d07db733c68931ca4ffe8c8dca6d1c
许大仙 <1900919313@qq.com>
2023-11-08 10:17:21 +0800

    Vue3 全家桶

--- 06-TypeScript/01_初识TypeScript/26_never类型.ts
+++ 06-TypeScript/01_初识TypeScript/26_never类型.ts
@@ -2,12 +2,15 @@
  * 处理 message
  * @param message
  */
-function handleMessage(message: string | number) {
+function handleMessage(message: string | number | boolean) {
     switch (typeof message) {
         case 'string':
             console.log(message.length)
             break
         case 'number':
+            console.log(message)
+            break
+        case 'boolean':
             console.log(message)
             break
         default:
@@ -20,5 +23,6 @@ function handleMessage(message: string | number) {
 
 handleMessage("abc")
 handleMessage(123)
+handleMessage(true)
 
 export {}
