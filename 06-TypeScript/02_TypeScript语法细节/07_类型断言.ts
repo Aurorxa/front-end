27ae1a3caadbeac5faf0a5f91fff94fdaf59e56e
许大仙 <1900919313@qq.com>
2023-11-08 14:19:22 +0800

    Vue3 全家桶

--- 06-TypeScript/02_TypeScript语法细节/07_类型断言.ts
+++ 06-TypeScript/02_TypeScript语法细节/07_类型断言.ts
@@ -1,4 +1,8 @@
 // 获取 DOM 元素 <img class="img">
 const imgEl = document.querySelector(".img") as HTMLImageElement
 
-console.log(imgEl.src)
+if (imgEl !== null) {
+    console.log(imgEl.src)
+}
+
+export {}
