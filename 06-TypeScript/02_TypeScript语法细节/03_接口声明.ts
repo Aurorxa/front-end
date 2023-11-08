51b9ebac003da107e87c342922f15345a16f8e33
许大仙 <1900919313@qq.com>
2023-11-08 13:39:57 +0800

    Vue3 全家桶

---
+++ 06-TypeScript/02_TypeScript语法细节/03_接口声明.ts
@@ -0,0 +1,12 @@
+interface Point {
+    x: number
+    y: number
+}
+
+function printPoint(point: Point): void {
+    console.log(point.x)
+    console.log(point.y)
+}
+
+
+export {}
---
+++ 06-TypeScript/02_TypeScript语法细节/04_类型别名VS接口声明.ts
@@ -0,0 +1,16 @@
+interface Point {
+    x: number
+    y: number
+}
+
+interface Point {
+    z?: number
+}
+
+function printPoint(point: Point): void {
+    console.log(point.x)
+    console.log(point.y)
+}
+
+
+export {}
