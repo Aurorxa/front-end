e47197d50026051c0a0a55f0b5c03a71f15e9f8e
许大仙 <1900919313@qq.com>
2023-11-09 14:31:35 +0800

    Vue3 全家桶

--- 06-TypeScript/02_TypeScript语法细节/22_函数的重载.ts
+++ 06-TypeScript/02_TypeScript语法细节/22_函数的重载.ts
@@ -2,14 +2,16 @@
 function add(arg1: number, args2: number): number
 function add(arg1: string, args2: string): string
 function add(arg1: string, args2: number): string
+function add(arg1: number, args2: string): string
 
 // 编写通用的函数实现
 function add(arg1: any, args2: any): any {
-    return arg1 + args2
+  return arg1 + args2
 }
 
-add("abc", "cba")
-add(1, 2)
-add("1", 2)
+console.log(add("abc", "cba"))
+console.log(add(1, 2))
+console.log(add("1", 2))
+console.log(add(1, "2"))
 
-export {}
+export { }
