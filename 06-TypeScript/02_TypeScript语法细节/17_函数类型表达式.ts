ac19983953ac54377344659ad1cf6c39b35ddddf
许大仙 <1900919313@qq.com>
2023-11-09 11:14:23 +0800

    Vue3 全家桶

--- 06-TypeScript/02_TypeScript语法细节/17_函数类型表达式.ts
+++ 06-TypeScript/02_TypeScript语法细节/17_函数类型表达式.ts
@@ -14,7 +14,7 @@ function add(num1: number, num2: number): number {
 
 calc(add)
 
-// TS 对于传入的函数类型的参数个数不进行检测（校验）
+// TS 对于传入的函数类型的多余的参数会被忽略掉
 function sub(num1: number, num2: number): number {
     return num1 - num2
 }
