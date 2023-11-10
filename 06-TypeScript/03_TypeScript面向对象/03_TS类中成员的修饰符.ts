9a31d62534850b39a10737b5a6ee87b1bcc8de3a
许大仙 <1900919313@qq.com>
2023-11-10 17:22:51 +0800

    TS

--- 06-TypeScript/03_TypeScript面向对象/03_TS类中成员的修饰符.ts
+++ 06-TypeScript/03_TypeScript面向对象/03_TS类中成员的修饰符.ts
@@ -1,11 +1,7 @@
 class Person {
   // 参数属性：TypeScript 提供了特殊的语法，用于将构造函数参数转换为具有相同名称和值的类属性。
   // 这些称为参数属性，是通过在构造函数参数前加上可见性修饰符 public、private、protected 或 readonly 之一来创建的。
-  constructor(private name: string, protected age: number, public gender: string) {
-    this.name = name
-    this.age = age
-    this.gender = gender
-  }
+  constructor(private name: string, protected age: number, public gender: string) {}
 
 }
 
