741be7a7cb808c0eecbca917c193d29b22030a51
许大仙 <1900919313@qq.com>
2023-12-25 15:07:31 +0800

    JSX语法

--- 07-React18全家桶/03_组件化开发/11_react-app-hoc/src/App.jsx
+++ 07-React18全家桶/03_组件化开发/11_react-app-hoc/src/App.jsx
@@ -1,26 +1,55 @@
 import React, {PureComponent} from 'react'
-import {Modal} from "@/components/Modal"
+import Modal from "@/components/Modal"
 import '@/App.css'
-import {createPortal} from "react-dom";
 
 class App extends PureComponent {
   
   state = {
     message: '我是 App 组件',
+    isShowModal: false,
   }
   
   showModal() {
-    createPortal(<Modal/>, document.querySelector('body'))
+    this.setState({
+      isShowModal: true,
+    })
+  }
+  
+  closeModal() {
+    this.setState({
+      isShowModal: false,
+    })
   }
   
   render() {
+    const {isShowModal} = this.state
     return (
-      <div className="container">
-        <div className="content">
-          <h2>{this.state.message}</h2>
-          <button onClick={() => this.showModal()}>点我，显示模态框</button>
+      <div >
+        <h2>{this.state.message}</h2>
+        <button onClick={() => this.showModal()}>点我，显示模态框</button>
+        <div className={`${isShowModal ? 'modal-open' : ''}`}>
+          {
+            isShowModal && (<Modal>
+              <div>
+                <div>
+                  <label htmlFor="username">
+                    用户名：<input type="text" id="username" defaultValue={""}/>
+                  </label>
+                </div>
+                <div>
+                  <label htmlFor="password">
+                    密码：<input type="password" id="password" defaultValue={""}/>
+                  </label>
+                </div>
+              </div>
+              <div>
+                <button onClick={() => this.closeModal()}>关闭模态框</button>
+              </div>
+            </Modal>)
+          }
         </div>
       </div>
+    
     )
   }
 }
--- 07-React18全家桶/03_组件化开发/11_react-app-hoc/src/components/Modal.jsx
+++ 07-React18全家桶/03_组件化开发/11_react-app-hoc/src/components/Modal.jsx
@@ -1,25 +1,16 @@
 import {PureComponent} from "react"
+import {createPortal} from "react-dom";
 
-export class Modal extends PureComponent {
+class Modal extends PureComponent {
   render() {
     return (
-      <div style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        margin: 'auto',
-        width: '300px',
-        height: '300px',
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        zIndex: 10000,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <button>关闭模态框</button>
-      </div>
+      createPortal(
+        (<div className="modal">
+          {this.props.children}
+        </div>)
+        , document.querySelector('body'))
     )
   }
 }
+
+export default Modal
