937ea3b492b88498a15f03bf75b1b4537708593c
许大仙 <1900919313@qq.com>
2023-12-25 16:18:59 +0800

    JSX语法

---
+++ 07-React18全家桶/04_React的过渡动画/01_react-app-css/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '@/App.css'
+import {CSSTransition} from "react-transition-group"
+
+class App extends React.PureComponent{
+  
+  state = {
+    message: '我是 App 组件',
+    isShow: false
+  }
+  
+  h2Ref = React.createRef()
+  change(){
+    this.setState({
+      isShow:!this.state.isShow
+    })
+  }
+  
+  render() {
+    const {isShow} = this.state
+    console.log(this.h2Ref.current)
+    return (
+      <div>
+        <button onClick={() => this.change()}>切换</button>
+        <CSSTransition in={isShow} nodeRef={this.h2Ref} classNames="h2" timeout={200} unmountOnExit>
+          <h2 ref={this.h2Ref}>{this.state.message}</h2>
+        </CSSTransition>
+      </div>
+    )
+  }
+}
+
+export default App
