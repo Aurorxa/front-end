53a7a0de7f68358c248a534ccd9b9d49f895be04
许大仙 <1900919313@qq.com>
2023-12-30 22:47:33 +0800

    优化

--- 07-React18全家桶/06_redux/02_react-app-redux/src/App.jsx
+++ 07-React18全家桶/06_redux/02_react-app-redux/src/App.jsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react'
-import Counter from "@/components/Counter";
+import Counter from "@/components/Counter"
+import Home from "@/components/Home"
 
 class App extends PureComponent {
   
@@ -8,6 +9,9 @@ class App extends PureComponent {
       <div>
         <div style={{background: "pink", padding: '10px', width: '500px'}}>
           <Counter/>
+        </div>
+        <div style={{background: "skyblue", padding: '10px', width: '500px'}}>
+          <Home/>
         </div>
       </div>
     )
