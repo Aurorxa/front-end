8b12bf7be3070069f5808f7ac2c2ec113c1899e1
许大仙 <1900919313@qq.com>
2024-01-01 09:08:11 +0800

    优化

---
+++ 07-React18全家桶/06_redux/04_react-app-redux/src/App.jsx
@@ -0,0 +1,19 @@
+import React, {PureComponent} from 'react'
+import '@/App.css'
+
+class App extends PureComponent {
+  
+  state = {}
+  
+  
+  render() {
+    const {isLogin} = this.state
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+}
+
+export default App
