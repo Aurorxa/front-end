be80c6923880745b14c55199aaac42b22aa9632b
许大仙 <1900919313@qq.com>
2024-01-05 16:38:32 +0800

    JSX语法

--- 07-React18全家桶/07_React路由/01_react-app-router/src/components/Home.jsx
+++ 07-React18全家桶/07_React路由/01_react-app-router/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react'
-
+import {withRouter} from "@/hoc/withRouter"
 
 class Home extends PureComponent {
   
@@ -8,13 +8,15 @@ class Home extends PureComponent {
   }
   
   render() {
+    const {router: {navigate}} = this.props
     const {message} = this.state
     return (
       <div>
         <h2>{message}</h2>
+        <button onClick={() => navigate("/about")}>跳转到 About 页面</button>
       </div>
     )
   }
 }
 
-export default Home
+export default withRouter(Home)
