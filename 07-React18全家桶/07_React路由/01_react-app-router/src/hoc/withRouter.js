fbb25735c783396f7e311ed50b57e1ea0445bf1f
许大仙 <1900919313@qq.com>
2024-01-05 16:39:23 +0800

    JSX语法

--- 07-React18全家桶/07_React路由/01_react-app-router/src/hoc/withRouter.js
+++ 07-React18全家桶/07_React路由/01_react-app-router/src/hoc/withRouter.js
@@ -1,7 +1,7 @@
 import {useNavigate} from "react-router-dom";
 
 export function withRouter(WrapperComponent) {
-  return function (props) {
+  return (props) => {
     const navigate = useNavigate()
     return (
       <WrapperComponent {...props} router={{navigate}}></WrapperComponent>
