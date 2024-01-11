18db4732aeb00b38583f818fbf91a377b8360338
许大仙 <1900919313@qq.com>
2024-01-11 10:12:10 +0800

    JSX语法

--- 07-React18全家桶/08_ReactHooks/05_react-app-hooks/src/App.jsx
+++ 07-React18全家桶/08_ReactHooks/05_react-app-hooks/src/App.jsx
@@ -1,31 +1,49 @@
-import {memo, useMemo, useState} from "react"
+import {forwardRef, memo, useImperativeHandle, useRef, useState} from "react"
 
-function calculateTotal(num) { // 计算和
-  let total = 0;
-  for (let i = 0; i < num; i++) {
-    total += i;
-  }
-  console.log('calculateTotal')
-  return total;
-}
 
-function App() {
+const Counter = memo(forwardRef((props, ref) => {
+  const [message] = useState("我是 Counter 组件")
   const [count, setCount] = useState(0)
   
-  const result = useMemo(() => {
-    return calculateTotal(100)
-  }, [])
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  
+  useImperativeHandle(ref, () => {
+    return {
+      handleClick: handleClick
+    }
+  }, [handleClick])
+  
+  return (
+    <div ref={ref}>
+      <h2>{message}</h2>
+      <h2>当前计数为：{count}</h2>
+      <button onClick={handleClick}>点击+1</button>
+    </div>
+  )
+}))
+
+function App() {
+  const [message] = useState("我是 App 组件")
+  const counterRef = useRef(null)
+  
+  
+  const handleClick = () => {
+    counterRef.current.handleClick()
+  }
   
   return (
     <div>
-      <div style={{background: 'pink'}}>
-        <h2>当前计数为：{count}</h2>
-        <h3>计算结果：{result}</h3>
-        <button onClick={() => setCount(count + 1)}>点我+1</button>
+      <div style={{background: 'pink', padding: '20px'}}>
+        <h2>{message}</h2>
+        <button onClick={handleClick}>点我+1</button>
+        <div style={{background: 'skyblue', padding: '20px', marginTop: '20px'}}>
+          <Counter ref={counterRef}/>
+        </div>
       </div>
     </div>
   )
 }
 
 export default memo(App)
-
