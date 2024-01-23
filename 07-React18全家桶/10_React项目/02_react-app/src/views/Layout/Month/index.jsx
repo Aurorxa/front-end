24f2d75f32c70b9b9ca35d97d05da1cfe96e9096
许大仙 <1900919313@qq.com>
2024-01-23 14:10:56 +0800

    完毕

--- 07-React18全家桶/10_React项目/02_react-app/src/views/Layout/Month/index.jsx
+++ 07-React18全家桶/10_React项目/02_react-app/src/views/Layout/Month/index.jsx
@@ -1,9 +1,12 @@
 import React, {memo} from "react"
+import {NavBar} from "antd-mobile";
 
 function Month() {
+  
+  
   return (
     <div>
-      我是月度
+    
     </div>
   )
 }
--- 07-React18全家桶/10_React项目/02_react-app/src/views/New/index.jsx
+++ 07-React18全家桶/10_React项目/02_react-app/src/views/New/index.jsx
@@ -1,9 +1,18 @@
 import React, {memo} from "react"
+import {NavBar, Toast} from "antd-mobile";
+import {useNavigate} from "react-router-dom";
 
 function New() {
+  const navigate = useNavigate()
+  const back = () => {
+    navigate(-1)
+  }
+  
   return (
     <div>
-      我是记账
+      <NavBar back='返回' onBack={back}>
+        标题
+      </NavBar>
     </div>
   )
 }
