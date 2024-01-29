066193387a00077272444212b4b3b693fcd83e32
许大仙 <1900919313@qq.com>
2024-01-29 13:57:16 +0800

    ts

---
+++ 06-TypeScript/02_TypeScript语法细节/27_ts中函数的类型.ts
@@ -0,0 +1,12 @@
+type getNumType = (num: number) => number
+/**
+ * 函数最主要的目的就是封装功能
+ */
+const getNum: getNumType = (num) => {
+  return num 
+}
+
+const res = getNum(10)
+console.log(res)
+
+export { }
