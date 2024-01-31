980c8212199af34d5a6b0bbb1c2285618dadbf8b
许大仙 <1900919313@qq.com>
2024-01-31 16:31:01 +0800

    ts

--- 06-TypeScript/03_TypeScript面向对象/07_TS中的setter和getter.ts
+++ 06-TypeScript/03_TypeScript面向对象/07_TS中的setter和getter.ts
@@ -1,22 +1,22 @@
 class Person {
-  #name: string = ""
+  private _name: string = ""
 
   constructor(name: string) {
-    this.#name = name;
+    this._name = name;
   }
 
   set name(name) {
-    this.#name = name
+    this._name = name
   }
 
   get name() {
-    return this.#name;
+    return this._name;
   }
 }
 
 const person = new Person("John");
 console.log(person.name);
 person.name = "Tom"
-console.log(person.name, person.#name) //
+console.log(person.name, person._name) // 报错，不可以在外部访问私有属性
 
 export { }
---
+++ 06-TypeScript/03_TypeScript面向对象/08_参数属性.ts
@@ -0,0 +1,19 @@
+class Person {
+  constructor(private _name: string, public age: number) {
+    this._name = _name;
+    this.age = age
+  }
+
+  set name(name) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+const person = new Person("John", 18);
+console.log(person.name, person.age); 
+
+export { }
