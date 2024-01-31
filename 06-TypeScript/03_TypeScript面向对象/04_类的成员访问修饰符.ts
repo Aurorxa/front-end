7d0db7e650498cb301c0bb9376bb98d01ae30b81
许大仙 <1900919313@qq.com>
2024-01-31 14:46:30 +0800

    ts

--- 06-TypeScript/03_TypeScript面向对象/04_类的成员访问修饰符.ts
+++ 06-TypeScript/03_TypeScript面向对象/04_类的成员访问修饰符.ts
@@ -1,7 +1,6 @@
 class Person {
   name: string
-  age: number
-
+  protected age: number // protected 在类自身及子类中可见、受保护的属性或方法
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
@@ -15,7 +14,6 @@ class Person {
     console.log(this.name + " is running");
   }
 }
-
 
 class Student extends Person {
   score: number = 0
