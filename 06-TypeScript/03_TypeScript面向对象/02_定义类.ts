1ea86277da948f784d4e422a24c1bc7ea5246843
许大仙 <1900919313@qq.com>
2024-01-31 14:14:10 +0800

    ts

--- 06-TypeScript/03_TypeScript面向对象/02_定义类.ts
+++ 06-TypeScript/03_TypeScript面向对象/02_定义类.ts
@@ -1,5 +1,5 @@
 class Person {
-  
+
   name: string = "" // 给类的成员变量标注类型注解，同时还可以赋予初始化值
   age: number = 0 // 给类的成员变量标注类型注解，同时还可以赋予初始化值
 
@@ -8,9 +8,14 @@ class Person {
     this.age = age;
   }
 
+  eating() {
+    console.log(this.name + " is eating");
+  }
+
 }
 
 const person = new Person('John', 20);
 console.log(person.name, person.age);
+person.eating()
 
 export { }
