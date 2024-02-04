c53140e45265f831e646e382473b435758601a04
许大仙 <1900919313@qq.com>
2024-02-04 16:06:00 +0800

    TS

---
+++ 06-TypeScript/03_TypeScript面向对象/20_枚举.ts
@@ -0,0 +1,22 @@
+enum Color { // 定义枚举
+  Red,
+  Green,
+  Blue
+}
+
+function getColor(color: Color) { // 参数是枚举类型
+  switch (color) {
+    case Color.Red: // 判断枚举
+      return '红色'
+    case Color.Green:
+      return '绿色'
+    case Color.Blue:
+      return '蓝色'
+  }
+}
+
+const color = getColor(Color.Green)
+
+console.log(color)
+
+export { }
